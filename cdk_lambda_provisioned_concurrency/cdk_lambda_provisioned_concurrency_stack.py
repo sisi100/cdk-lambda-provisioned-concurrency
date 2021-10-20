b2e657b2bfcda88c7ab98281d8355fa25ab009d0
Shun Isii <sisii00.swork@gmail.com>
2021-10-20 22:13:59 +0900

    新規追加

--- cdk_lambda_provisioned_concurrency/cdk_lambda_provisioned_concurrency_stack.py
+++ cdk_lambda_provisioned_concurrency/cdk_lambda_provisioned_concurrency_stack.py
@@ -1,15 +1,42 @@
-from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import core as cdk, aws_lambda, aws_applicationautoscaling as appscaling
+from datetime import datetime
 
 
 class CdkLambdaProvisionedConcurrencyStack(cdk.Stack):
-
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Lambdaを作る
+        hoge_lambda = aws_lambda.Function(
+            self,
+            "HogeLambda",
+            code=aws_lambda.Code.from_asset("src"),
+            handler="index.handler",
+            runtime=aws_lambda.Runtime.PYTHON_3_9,
+            architecture=aws_lambda.Architecture.ARM_64,  # プロビジョニングするのでARMできるだけ安くする
+        )
+
+        # 新しいバージョンを発番する
+        hoge_version = aws_lambda.Version(
+            self,
+            f"HogeVersion{datetime.today().timestamp()}",  # デプロイ毎にバージョンを作るためにIDを変化させる必要がある
+            lambda_=hoge_lambda,
+            removal_policy=cdk.RemovalPolicy.RETAIN,  # 過去バージョンを削除しないようにする
+        )
+
+        # 新しいバージョンからエイリアスを作る
+        hoge_alias = aws_lambda.Alias(self, "HogeAlias", alias_name="hoge-alias", version=hoge_version)
+
+        # エイリアスをオートスケールのターゲットに設定する
+        hoge_scaling = hoge_alias.add_auto_scaling(min_capacity=1, max_capacity=5)
+
+        # ターゲット追跡スケーリングする
+        hoge_scaling.scale_on_utilization(utilization_target=0.5)  # 使用率が50%を付近になるようにする
+
+        # スケジュールでスケーリングする
+        hoge_scaling.scale_on_schedule(
+            "Hoge1Schedule", schedule=appscaling.Schedule.cron(hour="0", minute="0"), max_capacity=5, min_capacity=2
+        )
+        hoge_scaling.scale_on_schedule(
+            "Hoge2Schedule", schedule=appscaling.Schedule.cron(hour="20", minute="0"), max_capacity=1, min_capacity=1
+        )
--- setup.py
+++ setup.py
@@ -8,36 +8,24 @@ with open("README.md") as fp:
 setuptools.setup(
     name="cdk_lambda_provisioned_concurrency",
     version="0.0.1",
-
     description="An empty CDK Python app",
     long_description=long_description,
     long_description_content_type="text/markdown",
-
     author="author",
-
     package_dir={"": "cdk_lambda_provisioned_concurrency"},
     packages=setuptools.find_packages(where="cdk_lambda_provisioned_concurrency"),
-
-    install_requires=[
-        "aws-cdk.core==1.128.0",
-    ],
-
+    install_requires=["aws-cdk.core==1.128.0", "aws-cdk.aws-lambda==1.128.0", "aws-cdk.aws_lambda_python==1.128.0",],
     python_requires=">=3.6",
-
     classifiers=[
         "Development Status :: 4 - Beta",
-
         "Intended Audience :: Developers",
-
         "Programming Language :: JavaScript",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-
         "Topic :: Software Development :: Code Generators",
         "Topic :: Utilities",
-
         "Typing :: Typed",
     ],
 )
